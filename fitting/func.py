5f65385527c7da1115b4ae6495973ff14b8d015e
o3u4 <149888279+o3u4@users.noreply.github.com>
2024-12-08 22:35:02 +0800

    add a function
    Add a function

--- fitting/func.py
+++ fitting/func.py
@@ -1,5 +1,6 @@
 import numpy as np
-from scipy.stats import kstest, norm, gamma
+from scipy.stats import norm, gamma, chi2, t, f
+
 
 # 一些常用分布  cdf: Cumulative Distribution Function 累积分布函数
 # pdf: Probability Density Function 概率密度函数 = 累积分布函数的积分
@@ -37,3 +38,41 @@ def weibull_pdf(x, a, k):
 
 def weibull_cdf(x, a, k):
     return 1 - np.exp(-(x / a) ** k)
+
+
+def chi2_pdf(x, df):
+    return chi2.pdf(x, df)
+
+
+def chi2_cdf(x, df):
+    return chi2.cdf(x, df)
+
+
+def t_pdf(x, df):
+    return t.pdf(x, df)
+
+
+def t_cdf(x, df):
+    return t.cdf(x, df)
+
+
+def f_pdf(x, df):
+    return f.pdf(x, df)
+
+
+def f_cdf(x, df):
+    return f.cdf(x, df)
+
+
+def period_envelope_func(t, f1, f2, period=1, init_phase=0):
+    """
+    生成包络震荡函数
+    :param t:
+    :param f1: 下包络
+    :param f2: 上包络
+    :param period: 震荡周期
+    :param init_phase: 初相
+    :return:
+    """
+    return (f1(t) + (f2(t) - f1(t)) *
+            (1 + np.sin(2 * np.pi * (t + init_phase) / period)) / 2)
